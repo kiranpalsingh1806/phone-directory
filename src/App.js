56368e01f3918637a0917c04c62d7c6260dadefa
kiranpalsingh1806 <kiranpalsingh1806@gmail.com>
2021-07-04 16:24:56 +0530

    Created 'Header' Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, {Component} from 'react';
+import Header from "./Header";
 
 class App extends Component{
   render(){
     return(
       <div>
-      // <div className="header">
-      // Phone Directory
-      // </div>
-      // <button> Add </button>
-      // <div>
-      //   <span> Name</span><br/>
-      //   <span> Phone </span>
-      // </div>
+         <Header/>
+         <button> Add </button>
+         <div>
+         <span> Name</span><br/>
+         <span> Phone </span>
+       </div>
       <label htmlFor="name">Name: </label>
       <input id="name" type="text" placeholder="Type Here" defaultValue="Shilpa"/>
-      
       </div>
       );
   }
