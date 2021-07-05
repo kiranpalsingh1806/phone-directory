cd46e6118aaeeb550b60627a2aae06476f61c42a
kiranpalsingh1806 <kiranpalsingh1806@gmail.com>
2021-07-05 13:00:02 +0530

    Added 'Phone Directory' as smart Component, renamed 'App' as 'ShowSubscribers' and converted 'AddSubscribers' and 'ShowSubscribers' as dumb components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import AddSubscriber from './AddSubscriber';
-import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './registerServiceWorker';
+import AddSusbscriber from './AddSubscriber';
 import './common/common.css';
+import PhoneDirectory from './PhoneDirectory';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<PhoneDirectory />, document.getElementById('root'));
+registerServiceWorker();
